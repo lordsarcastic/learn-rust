d1a1e96b3b34309255bb0867468478371236a699
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-05-01 17:31:24 +0100

    learned about ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    s.push_str(", worls!");
+
+    println!("{}", s);
+    println!("Working with freeing of space");
+
+    let s1 = String::from("Hello");
+    let s2 = s1;
+
+    //println!("{} is s2 while {} is s1", s2, s1); #will throw error if run
+
+    let s1 = String::from("Hello");
+    let s2 = s1.clone();
+
+    println!("{} is s2 while {} is s1", s2, s1);
+    println!("Using functions and scopes");
+
+    let s1 = gives_ownership();
+
+    let s2 = takes_and_gives_back(s1.clone());
+
+    println!("{} is s2 while {} is s1", s2, s1);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("Hello world");
+    some_string
+}
+
+fn takes_and_gives_back(a_string:String) -> String {
+    a_string
+}
