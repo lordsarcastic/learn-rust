e8e99115bb126293cb4068341c980f39b3c8fd1e
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-05-09 20:40:32 +0100

    Learned about enums

---
+++ enums/src/main.rs
@@ -0,0 +1,31 @@
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+/*
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route(ip_kind: IpAddrKind) {
+}
+*/
+
+fn main() {
+    println!("Hello, world!");
+
+    let four = IpAddrKind::V4(String::from(127, 0, 0, 1));
+    let six = IpAddrKind::V6(String::from("::1"));
+/*
+    let home = IpAddr {
+        kind: IpAddrKind:V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+*/
+}
