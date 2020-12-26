f15f62b3e5cd5c1d76d2e587b0619319a2caf7af
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-12-27 00:00:00 +0100

    Feature: Added love calculator

---
+++ love_calculator/src/main.rs
@@ -0,0 +1,42 @@
+use std::cmp::Ordering;
+use std::convert::TryInto;
+use std::io;
+
+
+fn get_input(var: &mut String) -> i32 {
+    io::stdin()
+        .read_line(var)
+        .expect("Failed to read line");
+
+    var.chars().count().try_into().expect("Length of name is too large!")
+}
+
+fn calculate_compatibility(male_count: i32, female_count: i32) -> f32 {
+    match male_count.cmp(&female_count) {
+        Ordering::Less => {
+            return ((male_count as f32 / female_count as f32) as f32 * 100.0) as f32;
+        },
+        Ordering::Greater => {
+            return ((female_count as f32 / male_count as f32) as f32 * 100.0) as f32;
+        },
+        Ordering::Equal => {
+            return 50.0;
+        }
+    }
+}
+
+
+fn main() {
+    let mut male = String::new();
+    let mut female = String::new();
+
+    println!("Enter male name: ");
+    let male_count = get_input(&mut male);
+
+    println!("Enter female name: ");
+    let female_count = get_input(&mut female);
+
+    let compat_value = calculate_compatibility(male_count, female_count).floor();
+    
+    println!("Love compatibility between {} and {} is {}%", male.trim(), female.trim(), compat_value);
+}
